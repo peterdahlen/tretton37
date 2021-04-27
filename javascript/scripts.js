a328d99bd6805c02fb91fb0bafd8060ca5a5b697
Peter Dahlén <36642622+peterdahlen@users.noreply.github.com>
2021-04-27 21:26:26 +0200

    Added filter by office functionality.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -15,10 +15,12 @@ $(document).ready(function() {
         // Map data and display it in a <div>
         const employees = data.map(employee => {
             return `
-            <div class="card">
-                <img class="profile" src="${employee.imagePortraitUrl}">
-                <p class="name">Name: ${employee.name}</p>
-                <p class="office">Office: ${employee.office}</p>
+            <div class="card ${employee.office}" tabindex="0">
+                <img class="profile" src="${employee.imagePortraitUrl}" alt="This is a beautiful profile picture of ${employee.name}" onerror="this.parentNode.style.display='none'">
+                <div class="info">
+                    <p class="name">Name: ${employee.name}</p>
+                    <p class="office">Office: ${employee.office}</p>
+                </div>
                 <div class="contact">
                     <a class="github" href="https://github.com/${employee.gitHub}" target="_blank">GitHub</a>
                     <a class="linkedin" href="https://www.linkedin.com/${employee.linkedIn}" target="_blank">LinkedIn</a>
@@ -33,6 +35,28 @@ $(document).ready(function() {
     // Button to toggle grid and list view
     $(".toggle-list").click(function(){
         $(".cards-wrapper-grid, .cards-wrapper-list").toggleClass("cards-wrapper-grid cards-wrapper-list");
+    })
+
+    // Buttons to filter employees by office
+    $("#filter-lund").click(function(){
+        $(".card").not(".Lund").hide();
+    })
+
+    $("#filter-helsingborg").click(function(){
+        $(".card").not(".Helsingborg").hide();
+    })
+
+    $("#filter-sthlm").click(function(){
+        $(".card").not(".Stockholm").hide();
+    })
+
+    $("#filter-borlange").click(function(){
+        $(".card").not(".Borlänge").hide();
+    })
+
+    $("#show-all").click(function(){
+        $(".card").show();
+        $(".card").has("img[src='null']").hide();
     })
 
 });
